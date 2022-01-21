6fed14b53a7886272f85af792bb42d732d601c35
linhltph17613 <linhlethuy1911@gmail.com>
2022-01-21 20:57:15 +0700

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": "plugin:react/recommended",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        semi: ["error", "always"],
+        quotes: ["error", "double"],
+        indent: ["error", 4],
+        "eol-last": ["error", "never"],
+        "linebreak-style": 0
+    },
+};
